8846f76c5c688550a0c6c1eb887e4ef6eb8c0887
BauTancredi <bautistatancredi@hotmail.com>
2020-04-18 18:48:29 -0300

    ModalContext

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModalContext } from "../context/ModalContext";
 
 const Recipe = ({ recipe }) => {
+  const { setIdRecipe } = useContext(ModalContext);
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header">{recipe.strDrink}</h2>
+        <img className="card-img-top" src={recipe.strDrinkThumb} alt="" />
+        <div className="card-body">
+          <button
+            type="button"
+            className="btn btn-primary btn-block"
+            onClick={() => {
+              setIdRecipe(recipe.idDrink);
+            }}
+          >
+            See recipe
+          </button>
+        </div>
       </div>
     </div>
   );
