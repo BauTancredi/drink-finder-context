442c48b0727656a8e6c231615911813260b87dca
BauTancredi <bautistatancredi@hotmail.com>
2020-04-18 19:27:33 -0300

    Modal recipe ingredients (WIP)

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,8 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Recipe = ({ recipe }) => {
-  const { setIdRecipe } = useContext(ModalContext);
+  const { setIdRecipe, recipeInfo, setRecipe } = useContext(ModalContext);
+
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const showIngredeintes = (recipeInfo) => {
+    let ingredients = [];
+    for (let i = 1; i < 16; i++) {
+      console.log("for");
+      if (recipeInfo[`strIngredients${i}`]) {
+        ingredients.push(
+          <li>
+            {recipeInfo[`strIngredients${i}`]} {recipeInfo[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredients;
+  };
 
   return (
     <div className="col-md-4 mb-3">
@@ -15,10 +66,33 @@ const Recipe = ({ recipe }) => {
             className="btn btn-primary btn-block"
             onClick={() => {
               setIdRecipe(recipe.idDrink);
+              handleOpen();
             }}
           >
             See recipe
           </button>
+
+          <Modal
+            open={open}
+            onClose={() => {
+              setIdRecipe(null);
+              handleClose();
+              setRecipe({});
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{recipeInfo.strDrink}</h2>
+              <h3 className="mt-4">Instructions</h3>
+              <p>{recipeInfo.strInstructions}</p>
+              <img
+                className="img-fluid my-4"
+                src={recipeInfo.strDrinkThumb}
+                alt=""
+              />
+              <h3>Ingredients</h3>
+              <ul>{showIngredeintes(recipeInfo)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
