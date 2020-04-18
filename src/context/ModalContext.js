1274b58f8ffbc9ded99e33f5c85b6b71e1aa4fbf
BauTancredi <bautistatancredi@hotmail.com>
2020-04-18 18:54:01 -0300

    fetchRecipe api

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -5,6 +5,16 @@ export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
   const [idRecipe, setIdRecipe] = useState(null);
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      if (!idRecipe) return;
+      const url = `https://thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+      const res = await axios(url).then((res) => setRecipe(res.data.drinks[0]));
+    };
+    fetchRecipe();
+  }, [idRecipe]);
 
   return (
     <ModalContext.Provider value={{ setIdRecipe }}>
